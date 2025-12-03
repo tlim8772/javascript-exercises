cff3916596d99c430711f40d55909f3777b99ae0
obrona <tlim8772@gmail.com>
2025-12-03 13:40:32 +0800

    solve exercise 1-5,16

--- 16_permutations/permutations.js
+++ 16_permutations/permutations.js
@@ -1,6 +1,15 @@
-const permutations = function() {
-  
-};
+function permutations(arr) {
+    if  (arr.length == 0) {
+        return [[]];
+    }
+
+    const out = []
+    for (let i = 0; i < arr.length; i++) {
+        const wish = permutations([...arr.slice(0, i), ...arr.slice(i + 1)]);
+        out.push(...wish.map(x => [arr[i], ...x]));
+    }
+    return out;
+}
   
 // Do not edit below this line
 module.exports = permutations;
